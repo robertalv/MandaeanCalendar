087e538780db6789074796d46ddb41e428d87d41
“farid.khafaji.zadeh” <farid.k.zadeh@gmail.com>
2021-01-24 08:30:48 -0500

    Added web

---
+++ android/app/src/main/kotlin/com/mandaean/mandaean_calendar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mandaean.mandaean_calendar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/farid.khafaji.zadeh/projects/docker/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/farid.khafaji.zadeh/projects/my_app_cal/mandaean_calendar"
+export "FLUTTER_TARGET=/Users/farid.khafaji.zadeh/projects/my_app_cal/mandaean_calendar/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_FRAMEWORK_DIR=/Users/farid.khafaji.zadeh/projects/docker/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=2.2.0"
+export "FLUTTER_BUILD_NUMBER=6"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/farid.khafaji.zadeh/projects/my_app_cal/mandaean_calendar/.dart_tool/package_config.json"
